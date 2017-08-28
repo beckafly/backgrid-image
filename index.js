e31c4332a3df055b6cf2b616bc622d67f7c25940
Alex Becker <alex.becker@shoebuy.com>
2017-08-28 13:57:33 -0400

    init amd

---
+++ index.js
@@ -0,0 +1,43 @@
+/*	
+ 	backgrid-image
+	https://github.com/beckafly/backgrid-image
+	Copyright (c)  2017 Alexander Becker
+  	Licensed under the MIT @license.
+*/
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(["backbone", "backgrid", "underscore"], factory);
+  } else if (typeof exports == "object") {
+    // CommonJS
+    module.exports = factory(require("backbone"), require("backgrid"), require("underscore"));
+  }
+  // Browser
+  else factory(root.Backbone, root.Backgrid, root._);
+
+}(this, function (Backbone, Backgrid, _) {
+
+	"use strict";
+
+	var ImageCell = Backgrid.Extention.ImageCell = Backbone.View.extend({
+
+		/** @property */
+		className: "image-cell",
+
+
+		/** Renders an image in a table cell. */
+
+		render: function() {
+			this.$el.empty();
+			this.$el.append(this.make_image_src(this.model));
+			this.delegateEvents();
+			return this;
+		},
+
+		make_image_src: function(model) {
+			var img_src = this.model.get("img_src");
+			return typeof img_src === "string" ? '<img src="' + img_src + '" width="50" alt="" />' : "no image avalable";
+		}
+
+	});
+}));
